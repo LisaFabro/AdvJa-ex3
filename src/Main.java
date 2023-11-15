1cdccf2ea7af31585aaa8dbf3ae8c1b557f238a1
LisaFabro <fabro.lisa@gmail.com>
2023-11-15 17:59:57 +0100

    add es 3 1.2

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,10 @@ public class Main {
 //Scrivere una funzione che provi a dividere un numero per 0 e
 // catturi l'eccezione leggendone il messaggio.
     public static void main(String[] args) {
+        int num1=2;
+        int num2=0;
         try{
-            int num1=2/0;
+            int result= num1/num2;
         }catch(Exception e){
             System.out.println("Cannot divide a number by zero");
         }
