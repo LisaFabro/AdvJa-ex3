d0737c02eef308c67e3fcecfdab0005575f76599
LisaFabro <fabro.lisa@gmail.com>
2023-11-16 17:01:33 +0100

    add es 3 spostata divisione e try catch in metodo esterno

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,17 @@
 public class Main {
 //Scrivere una funzione che provi a dividere un numero per 0 e
 // catturi l'eccezione leggendone il messaggio.
+
     public static void main(String[] args) {
-        int num1=2;
+        int num1=5;
         int num2=0;
-        try{
-            System.out.println("The result of the operation is: " + division(num1, num2));
-        }catch(ArithmeticException e){
+        division(num1,num2);
+    }
+    public static void division(int num1, int num2) {
+        try {
+            System.out.println("Il risultato della divisione: " + num1/num2);
+        } catch (ArithmeticException e) {
             System.out.println("Cannot divide a number by zero");
         }
-    }
-    public static int division(int num1, int num2){
-        return num1/num2;
     }
 }
